a35172557cbedc54334d9c8694335638b746e458
Sergei Kovalev <skovalev1902@gmail.com>
2022-08-16 16:03:22 +0300

    Initial Commit

---
+++ AutodocMvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutodocMvvm
+//
+//  Created by Sergei Kovalev on 16.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
